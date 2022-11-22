1a7f93d2afe53f807536523999f850d4feeb8627
weber1998 <krsz1998182000@gmail.com>
2022-11-22 11:55:33 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import Konyv from './Konyv';
+
+const Konyveim = [
+  {
+    id:1,
+    cim:"Malveil",
+    szerzo:"Robert Merle",
+    ar: 1111,
+    
+  },
+  {
+    id:2,
+    cim:"Védett férfiak",
+    szerzo:"Robert Merle",
+    ar: 2111,
+    
+  },
+  {
+    id:3,
+    cim:"Állati elmék",
+    szerzo:"Robert Merle",
+    ar: 3112,
+    
+  }
+]
+
+
+function App() {
+
+  function kosarFeldolgoz(adat) {
+    console.log(adat);
+  }
+
+
+  return ( // jsx - javascript + XML
+    <div className="App">
+      <header className="App-header">
+       <h1>Könyváruház</h1>
+      </header>
+      <article className="konyvek">
+      {
+        Konyveim.map((konyv, index)=>{
+            return(<Konyv konyvAdat={konyv} key={index} kosarKattintasMetodus={kosarFeldolgoz}/>)
+          }
+        )
+      }
+        
+
+       </article>
+       <footer className="lablec">
+        <p>Wéber Krisztián</p>
+       </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Konyv.js
@@ -0,0 +1,20 @@
+import './konyv.css';
+
+function Konyv(props) {
+
+    function kosarba() {
+        console.log(props.konyvAdat.id)
+        props.kosarKattintasMetodus(props.konyvAdat.id)
+    }
+
+    return(
+        <div className="konyv">
+            <h3>Cím: {props.konyvAdat.cim}</h3>
+            <p>Szerző: {props.konyvAdat.szerzo}</p>
+            <span>Ár: {props.konyvAdat.ar} FT </span>
+            <button onClick={kosarba}>Kosárba</button>
+        </div>
+    );
+}
+
+export default Konyv;
